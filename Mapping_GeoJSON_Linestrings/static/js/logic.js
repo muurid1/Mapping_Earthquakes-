4af21740f405d32f34b5937f9920c9a71aea888a
Souleymane Ndiaye <muurid@gmail.com>
2022-09-20 12:27:36 -0500

    commit

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,26 +1,26 @@
 // Add console.log to check to see if our code is working.
-console.log(“working”);
+console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map(“mapid”, {
+let map = L.map("mapid", {
     center: [44.0, -80.0],
     zoom: 4
   });
 
 
   // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer(‘https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}’, {
-    attribution: ‘Map data &copy; <a href=“https://www.openstreetmap.org/”>OpenStreetMap</a> contributors, <a href=“https://creativecommons.org/licenses/by-sa/2.0/”>CC-BY-SA</a>, Imagery (c) <a href=“https://www.mapbox.com/”>Mapbox</a>’,
+  let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: ‘mapbox/light-v10’,
+    id: 'mapbox/streets-v11',
     accessToken: API_KEY
   }).addTo(map);
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer(‘https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToxken}’, {
-attribution: ‘Map data © <a href=“https://www.openstreetmap.org/”>OpenStreetMap</a> contributors, <a href=“https://creativecommons.org/licenses/by-sa/2.0/”>CC-BY-SA</a>, Imagery (c) <a href=“https://www.mapbox.com/”>Mapbox</a>’,
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: ‘mapbox/dark-v10’,
+    id: 'mapbox/dark-v10',
     accessToken: API_KEY
 }).addTo(map);
 
@@ -34,11 +34,11 @@ let baseMaps = {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = “https://raw.githubusercontent.com/nataliepoorcreations/Mapping_Earthquakes/main/torontoRoutes.json”;
+let torontoData = "https://raw.githubusercontent.com/nataliepoorcreations/Mapping_Earthquakes/main/torontoRoutes.json";
 
 // Create a style for the lines.
 let myStyle = {
-    color: “#ffffa1",
+    color: "#ffffa1",
     weight: 2
 }
 
@@ -49,6 +49,6 @@ d3.json(torontoData).then(function(data) {
   L.geoJSON(data, {
     style: myStyle,
     onEachFeature: function(feature, layer) {
-        layer.bindPopup(“<h3> Airline: ” + feature.properties.airline + “</h3> <hr><h3> Destination: ” + feature.properties.dst + “</h3>“);
+        layer.bindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: " + feature.properties.dst + "</h3>");
     }
-};
+  });
